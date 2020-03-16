1e6fefd8e6582c30b74e4491ec808df414435054
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-03-16 11:00:46 -0400

    wip wip

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ function startingFunction(){
             name:"action"
         },
         {
-            messafe:"Select from the following options:",
+            message:"Select from the following options:",
             choices:["Department", "Employee", "Roles"],
             type:"list",
             name:"options"
@@ -75,7 +75,104 @@ function createData(options){
                             value: object.e_id
                         }
                     });
-                    
+
+                    employee.unshift({
+                        name: "no manager",
+                        value: null
+                    })
+
+                    inquirer.prompt([
+                        {
+                            name: "first_name",
+                            type:"input",
+                            message: "What is the employee's first name?"
+                        },
+                        {
+                            name: "last_name",
+                            type: "input",
+                            message:"What is the employee's last name?"
+                        },
+                        {
+                            name: "role",
+                            type: "list",
+                            message: "What is the employee's position?",
+                            choices: roles
+                        },
+                        {
+                            name: "manager",
+                            type: "list",
+                            message: "Who is the employee's manager?",
+                            choices: employees
+                        }
+                    ]).then(function(res){
+                        if(res.role === "N/A"){
+                            genRolePrompt();
+                        }else {
+                            console.log(`Adding ${res.first_name} ${res.last_name} as a new employee!`);
+                            console.log(res.manager);
+                            connection.query(
+                                "INSERT INTO employee SET?", {
+                                    first_name: res.first_name,
+                                    last_name: res.last_name,
+                                    role_id: res.role,
+                                    manager_id: res.manager
+                                },
+                                function(err, res){
+                                    if(err) throw(err);
+                                    console.log(res.affectedRows + "employee added!\n");
+                                    continuePrompt()
+                                }
+                            );
+                        }
+                    }).catch(function(err){
+                        console.log(err);
+                    })
+                })
+            })
+            break;
+
+        case "Role":
+            connection.query("SELECT * FROM department", function(err, res){
+                if(err) throw(err);
+
+                const departments = res.map(object => {
+                    return{
+                        name: object.departmentName,
+                        value: object.d_id
+                    }
+                });
+                departments.push("N/A")
+
+                inquirer.prompt([
+                    {
+                        name:"title",
+                        type: "input",
+                        message: "What is the new role?"
+                    },
+                    {
+                        name:"salary",
+                        type: "number",
+                        message:"What is the salary for that role?"
+                    },
+                    {
+                        name: "department",
+                        type: "list",
+                        message: "Which department does the employee work in?",
+                        choices: departments
+                    }
+                ]).then(function(res){
+                    if(res.department === "N/A"){
+                        genDepartmentPrompt();
+                    }else{
+                        console.log("Inserting a new role ...\n");
+                        connection.query(
+                            "INSERT INTO roles SET ?", {
+                                role_title: res.title,
+                                salary: res.salary,
+                                department_id: res.department
+                            },
+                        )
+                    }
                 })
             })
     }
